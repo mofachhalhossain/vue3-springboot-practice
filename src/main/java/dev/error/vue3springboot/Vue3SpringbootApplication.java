6536f3d35613897eb1425d58f1233e1aebef350f
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2022-01-11 20:46:18 +0600

    init

---
+++ src/main/java/dev/error/vue3springboot/Vue3SpringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.error.vue3springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vue3SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vue3SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/error/vue3springboot/Vue3SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.error.vue3springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vue3SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
